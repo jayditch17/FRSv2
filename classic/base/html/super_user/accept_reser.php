83fce7ddf5efb2bc815a289f8077c9f3a674a067
Jayditch Balansi <2164482@slu.edu.ph>
2020-01-06 00:17:54 +0800

    notif
    notif

--- classic/base/html/super_user/accept_reser.php
+++ classic/base/html/super_user/accept_reser.php
@@ -1,4 +1,48 @@
 <?php
+
+  require 'phpmailer/PHPMailerAutoLoad.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 4;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'frsitproject@gmail.com';                     // SMTP username
+    $mail->Password   = 'frs@itproject123';                               // SMTP password
+    $mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+    $mail->Port       = 587;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('frsitproject@gmail.com', 'FRS');
+    $mail->addAddress('frsitproject@gmail.com', 'Joe User');     // Add a recipient
+    //$mail->addAddress('ellen@example.com');               // Name is optional
+    $mail->addReplyTo('frsitproject@gmail.com', 'Information');
+
+    // Attachments
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Reservation/Request Accepted';
+    $mail->Body    = 'Your Reservation Request has been Accepted Thank You!';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+$mail->send();
+                                    echo '<script type="text/javascript">'; 
+                                    echo 'alert("Reservation is Accepted. Email Sent");'; 
+                                    echo 'window.location.href = "reservation.php";';
+                                    echo '</script>';    
+}catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+
+?>
+<?php
     include('functions.php');
     $eventID = $_GET['eventID'];
     $query = "select * from `request_su` where `eventID` = '$eventID'; ";
@@ -31,6 +75,7 @@
     }
     
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
--- classic/base/html/super_user/php_action/reject.php
+++ classic/base/html/super_user/php_action/reject.php
@@ -1,4 +1,49 @@
 <?php
+
+  require '../phpmailer/PHPMailerAutoLoad.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 4;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'frsitproject@gmail.com';                     // SMTP username
+    $mail->Password   = 'frs@itproject123';                               // SMTP password
+    $mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+    $mail->Port       = 587;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('frsitproject@gmail.com', 'FRS');
+    $mail->addAddress('frsitproject@gmail.com', 'Joe User');     // Add a recipient
+    //$mail->addAddress('ellen@example.com');               // Name is optional
+    $mail->addReplyTo('frsitproject@gmail.com', 'Information');
+
+    // Attachments
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Reservation/Request Rejected';
+    $mail->Body    = 'Your Reservation Request has been Rejected Thank You!';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+$mail->send();
+echo '<script type="text/javascript">'; 
+                                    echo 'alert("Reservation is Rejected. Email Sent");'; 
+                                    echo 'window.location.href = "../reservation.php";';
+                                    echo '</script>';    
+    
+}catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+
+?>
+<?php
 // Process delete operation after confirmation
 if(isset($_POST["eventID"]) && !empty($_POST["eventID"])){
     // Include config file
